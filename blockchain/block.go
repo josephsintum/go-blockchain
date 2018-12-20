8a2822c0951d891c269d277e9e3f3f591efd72be
josephsintum <joseph24jay@gmail.com>
2018-12-19 16:35:00 -0800

    adding badgerdb

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,9 +1,10 @@
 package blockchain
 
-// BlockChain contains array of Blocks
-type BlockChain struct {
-	Blocks []*Block
-}
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+)
 
 // Block is the struct of the block
 type Block struct {
@@ -25,19 +26,39 @@ func CreateBlock(data string, prevHash []byte) *Block {
 	return block
 }
 
-// AddBlock takes BlockChain and data string
-func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.Blocks[len(chain.Blocks)-1]
-	new := CreateBlock(data, prevBlock.Hash)
-	chain.Blocks = append(chain.Blocks, new)
-}
-
 // Genesis takes Block and returns a create block call
 func Genesis() *Block {
 	return CreateBlock("Genesis", []byte{})
 }
 
-// InitBlockChain takes BlockChain and returns an array of Blocks with a call to Genesis()
-func InitBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
+// Serialize takes block and returns bytes
+// doing this cause badgerdb takes only bytes and arrays
+func (b *Block) Serialize() []byte {
+	var res bytes.Buffer
+	encoder := gob.NewEncoder(&res)
+
+	err := encoder.Encode(b)
+
+	Handle(err)
+
+	return res.Bytes()
+}
+
+// Deserialize takes in data []byte and returns pointer to block
+func Deserialize(data []byte) *Block {
+	var block Block
+	decoder := gob.NewDecoder(bytes.NewReader(data))
+
+	err := decoder.Decode(&block)
+
+	Handle(err)
+
+	return &block
+}
+
+// Handle err
+func Handle(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,1 +1,11 @@
 module github.com/josephsintum/go-blockchain
+
+require (
+	github.com/AndreasBriese/bbloom v0.0.0-20180913140656-343706a395b7 // indirect
+	github.com/dgraph-io/badger v1.5.4
+	github.com/dgryski/go-farm v0.0.0-20180109070241-2de33835d102 // indirect
+	github.com/golang/protobuf v1.2.0 // indirect
+	github.com/pkg/errors v0.8.0 // indirect
+	golang.org/x/net v0.0.0-20181217023233-e147a9138326 // indirect
+	golang.org/x/sys v0.0.0-20181218192612-074acd46bca6 // indirect
+)
