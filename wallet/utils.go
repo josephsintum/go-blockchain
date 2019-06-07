82dcc97a480f8d5f1602d4ee4035c0c82e48b7eb
josephsintum <joseph24jay@gmail.com>
2019-06-07 11:50:09 -0700

    adding base58 utils func

---
+++ wallet/utils.go
@@ -0,0 +1,25 @@
+package wallet
+
+import (
+	"log"
+
+	"github.com/mr-tron/base58"
+)
+
+func Base58Encode(input []byte) []byte {
+	encode := base58.Encode(input)
+
+	return []byte(encode)
+}
+
+func Base58Decode(input []byte) []byte {
+	decode, err := base58.Decode(string(input[:]))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return decode
+}
+
+// base58 func does have 0 O 1 I + /
+// this reduces the chance of errors while copying str return like in a public key address
